adeffaff723da809a9c501935fef9afd2ffc63fd
samih freire <samihcisne@gmail.com>
2025-09-18 16:07:49 -0300

    feat: instalando e configurando json-serve, webpack, babel e o pacote day js

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict"
+
+import "./libs/dayjs"
+
+//CSS
+import "./styles/global.css"
+import "./styles/aside.css"
+import "./styles/body.css"
+import "./styles/main.css"
+import "./styles/form.css"
+import "./styles/footer.css"
