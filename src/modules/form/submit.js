f6953e7ba68992c2c61274a71b2e43ed2df80e18
samih freire <samihcisne@gmail.com>
2025-09-22 16:13:22 -0300

    feat: capturando informações de agendamento do formulário

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -2,6 +2,11 @@ import dayjs from "dayjs";
 
 const form = document.querySelector("form");
 const dateForm = document.getElementById("date-form");
+const inputFormTutorName = document.getElementById("form-tutor-name");
+const inputFormPetName = document.getElementById("form-pet-name");
+const inputFormPhone = document.getElementById("form-phone");
+const textAreaFormService = document.getElementById("form-service-description");
+const selectScheduleTime = document.getElementById("schedule-time");
 
 //Data atual para o input
 const inputToday = dayjs(new Date()).format("YYYY-MM-DD");
@@ -16,5 +21,63 @@ if(form != null){
     form.onsubmit = (event) => {
         //Previne o comportamento padrão do formulário
         event.preventDefault();
+
+        try {
+            //Recuperando o nome do tutor
+            const tutorName = inputFormTutorName.value.trim();
+
+            if(!tutorName) {
+                return alert("Por favor, preencha o nome do tutor.");
+            }
+
+            //Recuperando o nome do pet
+            const petName = inputFormPetName.value.trim();
+
+            if(!petName) {
+                return alert("Por favor, preencha o nome do pet.");
+            }
+
+            //Recuperando o telefone para contato
+            const phone = inputFormPhone.value.trim();
+
+            if(!phone) {
+                return alert("Por favor, preencha o telefone para contato.");
+            }
+
+            //Recuperando a descrição do serviço
+            const serviceDescription = textAreaFormService.value.trim();
+            if(!serviceDescription) {
+                return alert("Por favor, preencha a descrição do serviço.");
+            }
+
+            //Recuperando a horário selecionada
+            const hourSelected = selectScheduleTime.value;
+
+            if(!hourSelected) {
+                return alert("Por favor, selecione um horário para o agendamento.");
+            }
+
+            //Recupera somente a hora selecionada
+            const [hour] = hourSelected.split(":");
+            
+            //Insere a hora na data
+            const when = dayjs(dateForm.value).add(hour, "hour");
+            
+            const id = new Date().getTime();
+
+            console.log({
+                id,
+                tutorName,
+                petName,
+                phone,
+                serviceDescription,
+                when,
+            })
+
+        } catch (error) {
+            alert("Não foi possível realizar o agendamento.");
+            console.log(error)
+        }
     }
 }
+
