d74ee18ca9410abb910d1db00f1433b263ee4257
samih freire <samihcisne@gmail.com>
2025-09-22 16:14:58 -0300

    feat: criado mascara para o input de telefone

--- src/modules/page-load.js
+++ src/modules/page-load.js
@@ -1,5 +1,24 @@
 import { schedulesDay } from "./schedules/load.js"
 
+const formPhone = document.getElementById("form-phone");
+
 document.addEventListener("DOMContentLoaded", function() {
     schedulesDay();
 });
+
+formPhone.addEventListener("input", (event) => {
+    handlePhone(event);
+});
+
+function handlePhone(event) {
+  let input = event.target
+  input.value = phoneMask(input.value)
+}
+
+const phoneMask = (value) => {
+  if (!value) return ""
+  value = value.replace(/\D/g,'')
+  value = value.replace(/(\d{2})(\d)/,"($1) $2")
+  value = value.replace(/(\d)(\d{4})$/,"$1-$2")
+  return value
+}
